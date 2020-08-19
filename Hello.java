3112ef39c67e4db57c87c480cbd08bc3eb2cf710
ELJEFEXIRL <69922436+ELJEFEXIRL@users.noreply.github.com>
2020-08-19 15:18:50 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,25 @@
+/**********************************
+* Hello Java
+***********************************
+** Isabella Larkin
+*8/19/20
+*CMSC 255 002
+***********************************/
+
+public class Hello
+{ 
+   public static void main(String[ ] args)
+   { 
+      System.out.println("Isabella Larkin"); 
+      
+      System.out.println("Hello Java");
+      
+      System.out.println("This is a program named hello that outputs mt student data I entered");
+      
+      System.out.println("8/19/20 V1");
+      
+      System.out.println("CMSC 255 OO2");
+      
+      System.out.println();
+   }
+}
